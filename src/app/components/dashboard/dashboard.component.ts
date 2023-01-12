8638df86e0e2cfb62d65cd7ec62fa55619135916
Mathou54 <matt54425+github.com@gmail.com>
2023-01-12 20:09:36 +0100

    chore(npm): update dependencies

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
 import {Observable, of} from 'rxjs';
 import {Service} from '../../model/service';
 import {ServiceService} from '../../services/service.service';
@@ -14,12 +14,12 @@ export class DashboardComponent implements OnInit {
 
 	services: Observable<Service[]>;
 
-	controls: FormGroup;
+	controls: UntypedFormGroup;
 
 	refreshCount: number;
 
-	constructor(private fb: FormBuilder,
-				private service: ServiceService) {
+	constructor(private fb: UntypedFormBuilder,
+	            private service: ServiceService) {
 	}
 
 	ngOnInit() {
